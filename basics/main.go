c6c18262d9648e3b2e23ee13709b95c36f9bd051
ShashikaUdara <techdudelab@gmail.com>
2022-05-11 23:59:27 +0530

    more basics - variable declarations

--- basics/main.go
+++ basics/main.go
@@ -5,13 +5,36 @@ import "fmt"
 func main(){
 	// its main
 	var num1, num2, sum int;
+	var fl1, fl2, flSum float32;
+	var character, level byte;
+
+	// you can use direct declaration, the compilar will identify the correct data type itself.
+	number1 = 4.25;
+	number2 = 45.25;
+	sumNum = 0;
 
 	// checking sum before assigning values
 	sum = num1 + num2;
 	fmt.Println(sum);
 
 	// checking values after the assignment
-	num1 = 12,	num2 = 15;
+	// integers
+	num1 = 12; num2 = 15;
 	sum = num1 + num2;
 	fmt.Println(sum);
+
+	// floats
+	fl1 = 4.12; 
+	fl2 = 45.89; 
+	flSum = fl1 + fl2;
+	fmt.Println(flSum);
+
+
+	// bytes
+	character = 180;
+	level = character - 10;
+	fmt.Println(level);
+
+	sumNum = number1 + number2
+	fmt.Println(sumNum);
 }
