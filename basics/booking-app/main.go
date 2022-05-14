d3000538c5f702ebc7c5dadd857a283963147220
ShashikaUdara <techdudelab@gmail.com>
2022-05-14 21:01:29 +0530

    user input validation

--- basics/booking-app/main.go
+++ basics/booking-app/main.go
@@ -31,6 +31,7 @@ func main() {
 		var firstName string
 		var lastName string
 		var email string
+		var city string
 		var userTickets uint
 		// ask for user name
 
@@ -46,23 +47,60 @@ func main() {
 		fmt.Print("Enter your email address : ")
 		fmt.Scan(&email)
 
+		fmt.Print("Enter your city : ")
+		fmt.Scan(&city)
+
 		fmt.Print("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		bookings = append(bookings, firstName+" "+lastName)
+		// validating the user inputs
+		isValidUser := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		isValidCity := city == "Colombo" || city == "Kandy"
 
-		remainingTickets = remainingTickets - userTickets
+		if isValidUser && isValidEmail && isValidTicketNumber && isValidCity {
+			bookings = append(bookings, firstName+" "+lastName)
 
-		// userName = "Udara"
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will revceve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+			remainingTickets = remainingTickets - userTickets
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			// userName = "Udara"
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will revceve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("First names in Bookings %v\n\n", firstNames)
+
+			// var noTicketsRemaining bool = remainingTickets == 0 // or
+			noTicketsRemaining := remainingTickets == 0
+
+			if noTicketsRemaining {
+				// end the program
+				fmt.Println("Come next year")
+
+				break
+			}
+		} else {
+			if !isValidUser {
+				fmt.Println("The input first name or the last name is too short!")
+			}
+			if !isValidEmail {
+				fmt.Println("The input email is not in the correct order!")
+			}
+			if !isValidTicketNumber {
+				fmt.Printf("You can't buy %v tickets, we have only %v tickets left!\n", userTickets, remainingTickets)
+			}
+			if !isValidCity {
+				fmt.Println("You are in a wrong city!")
+			}
+			// fmt.Println("Somthing is not right")
+			// fmt.Printf("You can't have %v amount of tickets, the number of remaining tickets are %v\n\n", userTickets, remainingTickets)
+			continue
 		}
-
-		fmt.Printf("First names in Bookings %v\n\n", firstNames)
 	}
 }
