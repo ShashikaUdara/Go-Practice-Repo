d26d02b6565c546163fb94ac9152e155fa0088dc
ShashikaUdara <techdudelab@gmail.com>
2022-05-15 14:48:44 +0530

    package level varible, bad practice

--- basics/booking-app/main.go
+++ basics/booking-app/main.go
@@ -5,12 +5,23 @@ import (
 	"strings"
 )
 
-func main() {
-	// can't do this for const values
-	conferenceName := "Go Conference" // error because of not using the variable
+// package level variables : normaly this is a bad practice
+// define variables as much as possible in locally, not in publicaly
+const conferenceTickets uint = 50
 
-	const conferenceTickets uint = 50
-	var remainingTickets uint = 50
+var conferenceName string = "Go Conference"
+var remainingTickets uint = 50
+var bookings []string
+
+func main() {
+	// function level variables
+	// can't do this for const values
+	// var conferenceName string= "Go Conference" // error because of not using the variable
+	// const conferenceTickets uint = 50
+	// var remainingTickets uint = 50
+	// var bookings []string // or
+	// var bookings = []string{} // or
+	// bookings := []string[] // this
 
 	// checking types
 	fmt.Printf("conferenceName is %T, conferenceTickets is %T and remainingTickets is %T.\n", conferenceName, conferenceTickets, remainingTickets)
@@ -23,10 +34,6 @@ func main() {
 	// var bookings [50]string // this is a simmilar convention for the above statment
 
 	// slices insterd of arrays
-	var bookings []string // or
-	// var bookings = []string{} // or
-	// bookings := []string[] // this
-
 	// function
 
 	for {
@@ -45,11 +52,11 @@ func main() {
 		}
 
 		// validating the user inputs
-		isValidUser, isValidEmail, isValidTicketNumber, isValidCity := validateUserInput(firstName, lastName, email, userTickets, remainingTickets, city)
+		isValidUser, isValidEmail, isValidTicketNumber, isValidCity := validateUserInput(firstName, lastName, email, userTickets, city)
 
 		if isValidUser && isValidEmail && isValidTicketNumber && isValidCity {
 			// booking ticket
-			bookings, remainingTickets = bookTicket(bookings, firstName, lastName, remainingTickets, userTickets, conferenceName, email)
+			bookings, remainingTickets = bookTicket(firstName, lastName, userTickets, email)
 
 			// greeting to user
 			greetUser(conferenceName)
@@ -62,7 +69,7 @@ func main() {
 
 			// calling only by first name
 			// firstNames [] string
-			firstNames := getFirstNames(bookings)
+			firstNames := getFirstNames()
 			fmt.Printf("First names in Bookings %v\n\n", firstNames)
 
 			noTicketsRemaining := remainingTickets == 0
@@ -97,7 +104,7 @@ func greetUser(confName string) {
 	fmt.Printf("Welcome to our %v", confName)
 }
 
-func getFirstNames(bookings []string) []string {
+func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
 		var names = strings.Fields(booking)
@@ -108,7 +115,7 @@ func getFirstNames(bookings []string) []string {
 	return firstNames
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint, city string) (bool, bool, bool, bool) {
+func validateUserInput(firstName string, lastName string, email string, userTickets uint, city string) (bool, bool, bool, bool) {
 	isValidUser := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
@@ -146,11 +153,11 @@ func getUserInput() (string, string, string, string, uint) {
 	return firstName, lastName, email, city, userTickets
 }
 
-func bookTicket(bookings []string, firstName string, lastName string, remainingTickets uint, userTickets uint, confName string, email string) ([]string, uint) {
+func bookTicket(firstName string, lastName string, userTickets uint, email string) ([]string, uint) {
 	bookings = append(bookings, firstName+" "+lastName)
 	remainingTickets = remainingTickets - userTickets
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will revceve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, confName)
+	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
 
 	return bookings, remainingTickets
 }
